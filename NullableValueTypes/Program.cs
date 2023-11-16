cb4fd2dceae7f99fe5770d05f7564031133dba93
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-16 18:37:06 +0200

    samples with nullable value types

---
+++ NullableValueTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NullableValueTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<bool> flag = false;
+
+            Console.WriteLine(flag.GetValueOrDefault(true));
+
+
+            int? nullableInt = null;
+            nullableInt.GetValueOrDefault(-10);
+
+
+        }
+    }
+}
